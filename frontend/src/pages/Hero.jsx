9377cb8dc4f5fe9c93804052c0ce215691698ae7
Jerydam <jerydan148@gmail.com>
2023-04-28 23:47:01 +0100

    desc.

---
+++ frontend/src/pages/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div classNane="" >
+        <img src="/house.jpg" alt="" />
+        <div className="flex card h-[100px]">
+          <p>
+            <h5>price</h5>
+          <h1>1,70000</h1>
+          </p>
+          <button className='rounded bg-green-600 p-2'>Send Request</button>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Hero
