5212b2465db23a35f1de0111d625c690bbc48092
Jerydam <jerydan148@gmail.com>
2023-04-28 14:17:57 +0100

    hero

---
+++ frontend/src/constant/index.js
@@ -0,0 +1,119 @@
+export const exploreEvents = [
+  {
+    id: 'house-1',
+    imgUrl: '/house.jpg',
+    title: 'World cup Final',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/ile.jpg',
+    title: 'Facebook Seminar',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/ibugbe.jpg',
+    title: 'Timeless Album Launching',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/event4.jpg',
+    title: 'Web3bridge Conference',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/event5.jpg',
+    title: 'Burna Boy Concert',
+  },
+];
+
+export const startingFeatures = [
+  'Event organizers create an event on the platform and set the ticket prices, quantity, and other details.',
+  'The event is published on the platform and attendees can browse and purchase tickets using cryptocurrency.',
+  'The tickets are stored on the blockchain as unique, non-fungible tokens (NFTs) that represent ownership rights.',
+  'Attendees can transfer, resell, or gift their tickets to other users on the platform, without the need for intermediaries or authorization from the organizer.',
+  'Attendees get proof of attendance certificate which are unique NFTs courtesy of MetaSquare',
+  'Smart contracts automatically enforce the terms and conditions of the ticket sale, such as refund policies, transfer restrictions, and event cancellations.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/blockchain.png',
+    title: 'Decentralization',
+    subtitle:
+        'Our platform is fully decentralized, meaning that there are no central authorities or intermediaries controlling the ticket sales, transfers, or ownership',
+  },
+  {
+    imgUrl: '/nft.png',
+    title: 'Non-fungible tokens (NFTs)',
+    subtitle:
+        'Unlike traditional ticketing solutions, we use NFTs to represent tickets on the blockchain',
+  },{
+    imgUrl: '/smart-contracts.png',
+    title: 'Smart contracts',
+    subtitle:
+        'Our platform uses smart contracts to automate the ticketing process and ensure that all parties comply with the agreed-upon terms and conditions. ',
+  },{
+    imgUrl: '/peer-to-peer.png',
+    title: 'Peer-to-peer',
+    subtitle:
+        'Our platform enables event organizers and attendees to interact directly, without the need for intermediaries such as ticketing agencies or marketplaces. ',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/illustre.webp',
+    title: 'Transparency',
+    subtitle:
+        'With our platform, event organizers and attendees can benefit from increased transparency in the ticketing process',
+  },
+  {
+    imgUrl: '/iFlexi.jpg',
+    title: 'Flexibility',
+    subtitle:
+        'Our platform provides event organizers with greater flexibility in setting ticket prices, quantities, and other details.',
+  },
+  {
+    imgUrl: '/trans.webp',
+    title: 'Security',
+    subtitle:
+        'By leveraging blockchain technology and NFTs, our platform provides a high level of security and protection to event organizers and attendees.',
+  },
+  {
+    imgUrl:'/iEffi.jpg',
+    title:'Efficiency',
+    subtitle:'Our platform enables event organizers and attendees to interact directly, without the need for intermediaries or middlemen. '
+  },
+  {
+    imgUrl:'/innova.jpg',
+    title:'Innovation',
+    subtitle:'Our platform represents a novel and innovative approach to event ticketing that leverages cutting-edge technologies such as blockchain, NFTs, and smart contracts.'
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+  {
+    name: 'discord',
+    url: '/discord.png',
+  },
+  {
+    name: 'github',
+    url: '/github.png',
+  },
+];
