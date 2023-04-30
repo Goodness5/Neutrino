7f2f60d604cc3241190ade2d6454d6a8c665dc90
goodness5 <goodnesskolapo@gmail.com>
2023-04-30 17:26:19 +0100

    frontend deploys

---
+++ frontend/src/components/Buy/Propertydetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PropertyDetails = ({ number, text, info }) => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <span className="text-xl">{number}</span>
+        <h3 className="font-bold">{text}</h3>
+        <p>{info}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyDetails;
