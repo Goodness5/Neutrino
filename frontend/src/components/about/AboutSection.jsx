aee8365a157068867766d8581d5cfaff1359fb66
dadsec-dev <secdad1@gmail.com>
2023-05-01 00:48:05 +0100

    updated about page with styling and responsiveness

--- frontend/src/components/about/AboutSection.jsx
+++ frontend/src/components/about/AboutSection.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import heroImage from "../../images/Rectangle.png";
+import styling from "../../../styles/Home.module.css";
 
 function AboutSection() {
   return (
     <div>
-      <div className="containerabout">
-        <div className="leftone">
+      <div className={`${styling.containerabout} ${styling.resp}`}>
+        <div className={`${styling.leftone} ${styling.hideonmobile}`}>
           <Image src={heroImage} alt="hero-image" />
         </div>
 
-        <div className="rightone">
-          <h2 className="aboutintroh2">
+        <div className={`${styling.rightone} ${styling.alignleft}`}>
+          <h2 className={styling.aboutintroh2}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </h2>
-          <p className="sectionp">
+          <p className={styling.sectionp}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quae
             itaque magnam nemo veritatis asperiores? Commodi earum quas minus
             cum vero libero voluptatem. Vero, aut quibusdam harum accusamus
