9b4cc5c60840d15906fca6e16eeff49aa1638268
Faytey7 <Faithroberts708@gmail.com>
2023-05-03 16:05:28 +0100

    adding nft images

--- frontend/src/pages/Sell.jsx
+++ frontend/src/pages/Sell.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Sell = () => {
   return (
-    <div className='bg-[#000000] h-[20%]'>
-            <div className='flex w-[50%] h-[50%] bg-[#000000]'> 
-            <h1 className='text-[300px]'>Work</h1>
-            </div> 
+    <div className="bg-[#000000] h-[20%]">
+      <div className="flex w-[50%] h-[50%] bg-[#000000]">
+        <h1 className="text-[300px] text-white">Work</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sell
+export default Sell;
 
-
-
-
-{/* <div className='flex w-[50%]'>
+{
+  /* <div className='flex w-[50%]'>
                     <div className='w-[20%]'><h1 className='text-2xl w-9'>1</h1></div>
                     <div className='w-[20%]'><h1 className='text-2xl'>2</h1></div>
                     <div className='w-[20%]'><h1 className='text-2xl'>3</h1></div>
@@ -26,4 +24,5 @@ export default Sell
                     <div>Deposit NFT</div>
                     <div>Create Fraction</div>
                     <div>Approve Fraction</div>
-                </div> */}
+                </div> */
+}
