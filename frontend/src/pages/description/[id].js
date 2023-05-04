280f4b760ca7d614a0145f755a29327906a59dae
goodness5 <goodnesskolapo@gmail.com>
2023-05-04 21:26:58 +0100

    okay

--- frontend/src/pages/description/[id].js
+++ frontend/src/pages/description/[id].js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { neutrinoEstate } from "../../utils/contractInfo";
-import estateAbi from "../utils/neutroAbi.json";
+import estateAbi from "../../utils/neutroAbi.json";
 import {
   useAccount,
   useContractRead,
