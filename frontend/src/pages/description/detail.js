7cf3f12c6ad220ee5972fa0ce0dbbd363a6a466b
goodness5 <goodnesskolapo@gmail.com>
2023-05-04 21:38:41 +0100

    oo

--- frontend/src/pages/description/detail.js
+++ frontend/src/pages/description/detail.js
@@ -1,17 +1,17 @@
-// import DisplayNFT from "../../components/sell/DisplayNFT";
-// import Image from "next/image";
-// import { useRouter } from "next/router";
-// import React, { useEffect, useState } from "react";
-// import { useContractRead } from "wagmi";
-// import { neutrinoEstate } from "../../utils/contractInfo";
+import DisplayNFT from "../../components/sell/DisplayNFT";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useContractRead } from "wagmi";
+import { neutrinoEstate } from "../../utils/contractInfo";
 
   
   
   
-//   const { id } = useRouter().query;
-//   const ID = Number(id) - 1;
+  const { id } = useRouter().query;
+  const ID = Number(id) - 1;
 
-//   const [allProperties, setAllProperties] = useState();
+  const [allProperties, setAllProperties] = useState();
 
 //   const { data, isLoading, isError } = useContractRead({
 //     address: "0x1f6feeed3fb9696a5fb3a6ab78b5b3c7e1eb2f5f",
@@ -26,15 +26,15 @@
 
 
 // export const descDetails = [
-//   {
-//     id: {allProperties?.[ID],
+
+//     id: {allProperties?.[ID]},
 //     imgUrl: '/house.jpg',
 //     price:'1eth',
 //     bathroom:'4',
 //     bedroom: '3',
 //     houseType:'duplex',
 //     houseDesc:'Event organizers create an event on the platform and set the ticket prices, quantity, and other details.'
-//   },
+//   ,
   
 // ];
 
@@ -60,12 +60,12 @@
 //   },
 // ];
 
-// export const propertyValution = [
-//   {
-//     PropertyTypes:'Location rating',
-//     crimeLevel:'Content',
-//     ecologicalState:'1981',
-//     noiseLevel:'7800 club',
-//     infractureAssessment:'345'
-// },
-// ];
+export const propertyValution = [
+  {
+    PropertyTypes:'Location rating',
+    crimeLevel:'Content',
+    ecologicalState:'1981',
+    noiseLevel:'7800 club',
+    infractureAssessment:'345'
+},
+];
